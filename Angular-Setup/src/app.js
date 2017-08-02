d9de8fdf651a9639272074a99a5c5076a6900d63
Prassanjeet Singh <carbondating@Prassanjeets-MacBook-Pro.local>
2017-08-02 19:14:49 +0530

    AJS learnt till part2

--- Angular-Setup/src/app.js
+++ Angular-Setup/src/app.js
@@ -2,6 +2,11 @@
 
 angular.module('myApp',[]);
 
-angular.module('myApp').controller('MainController',['$scope',function($scope){
-    console.log('Hello!');
+angular.module('myApp').controller('MainController',[function(){
+    this.user = {
+        firstName: 'John',
+        lastName: 'Dorian',
+        accountType: 'CHECKING',
+        balance: 1349.2
+    };
 }]);
