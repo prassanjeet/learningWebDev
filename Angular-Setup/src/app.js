d12cbee117107e7d2141fac2b8aab9134fce8e2e
Singh Dhanjal <p.singh.dhanjal@accenture.com>
2017-08-02 17:05:48 +0530

    Part2 of AJS completed

--- Angular-Setup/src/app.js
+++ Angular-Setup/src/app.js
@@ -2,53 +2,11 @@
 
 angular.module('myApp',[]);
 
-angular.module('myApp').controller('MainController',['$scope','$interval',function($scope,$interval){
-    //part0
-    console.log('Hello!');
-
-    //part1
-    $scope.message = "Hello";
-
-    $scope.sayHello = function(name){
-        return $scope.message + ' ' + name + '.';
+angular.module('myApp').controller('MainController',[function(){
+    this.user = {
+        firstName: John,
+        lastName: Dorian,
+        accountType: 'CHECKING',
+        balance: 1349.2
     };
-
-    //part2
-    var items = ['bananas','apples','pears','cherries','peaches'];  // private variable
-    $scope.itemIndex = null;
-    $scope.currentItem = '';
-
-    $scope.getItem = function(){
-        $scope.currentItem = items[$scope.itemIndex];
-    };
-
-    $interval(function(){
-
-        $scope.itemIndex = Math.round(Math.random()*(items.length - 1));
-        $scope.getItem();
-    }, 2000);   //2000 milliseconds
-
-    //part3
-    $scope.randomValue = -999;
-
-    $scope.names = ['David','Thomas','Joe'];
-
-    $scope.qty = 20;
-    $scope.cost = 1.99;
-
-    $scope.pWidth = 100;
-
-    $interval(function(){
-        $scope.randomValue = Math.round(Math.random()*1000000);
-    }, 2000);
-    
-}]);
-    
-//part4
-angular.module('myApp').controller('ParentController',['$scope',function($scope){
-    $scope.name = 'Nathha Shikari';
-    
-}]);
-angular.module('myApp').controller('ChildController',['$scope',function($scope){
-    
 }]);
