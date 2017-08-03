862a33e96cd14673e3734444b01eafcbf99b3aef
Singh Dhanjal <p.singh.dhanjal@accenture.com>
2017-08-03 19:00:18 +0530

    end of day2

--- Angular-Setup/src/app.js
+++ Angular-Setup/src/app.js
@@ -10,3 +10,22 @@ angular.module('myApp').controller('MainController',[function(){
         balance: 1349.2
     };
 }]);
+
+// custom filters
+angular.module('myApp').filter('capitalize',function(){
+
+    return function(value){
+        var result = null;
+        var words = value.split(' ');
+        words.forEach(function(item) {
+            if(result){
+                result += '.';
+            }else{
+                result = '';
+            }
+            result += item.substr(0,1).toUpperCase() + item.substr(1).toLowerCase();
+        },);
+        return result;
+    };
+
+});
