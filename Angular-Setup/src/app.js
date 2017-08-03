2307d41307d740470436de249b0a53cb6390b1c9
Singh Dhanjal <p.singh.dhanjal@accenture.com>
2017-08-03 09:48:44 +0530

    day 2 start

--- Angular-Setup/src/app.js
+++ Angular-Setup/src/app.js
@@ -4,8 +4,8 @@ angular.module('myApp',[]);
 
 angular.module('myApp').controller('MainController',[function(){
     this.user = {
-        firstName: John,
-        lastName: Dorian,
+        firstName: 'John',
+        lastName: 'Dorian',
         accountType: 'CHECKING',
         balance: 1349.2
     };
