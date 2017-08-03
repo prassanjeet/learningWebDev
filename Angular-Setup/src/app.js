cf523af72b959e9944fcaa1b19bff3fe6c17bb65
Singh Dhanjal <p.singh.dhanjal@accenture.com>
2017-08-03 10:22:14 +0530

    Merging the changes from home

--- Angular-Setup/src/app.js
+++ Angular-Setup/src/app.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 "use strict";
 
 angular.module('myApp',[]);
@@ -11,17 +10,3 @@ angular.module('myApp').controller('MainController',[function(){
         balance: 1349.2
     };
 }]);
-=======
-"use strict";
-
-angular.module('myApp',[]);
-
-angular.module('myApp').controller('MainController',[function(){
-    this.user = {
-        firstName: 'John',
-        lastName: 'Dorian',
-        accountType: 'CHECKING',
-        balance: 1349.2
-    };
-}]);
->>>>>>> d9de8fdf651a9639272074a99a5c5076a6900d63
--- Angular-Setup/src/intro to AJS.js
+++ Angular-Setup/src/intro to AJS.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 "use strict";
 
 angular.module('myApp',[]);
@@ -54,61 +53,4 @@ angular.module('myApp').controller('ParentController',['$scope',function($scope)
 }]);
 angular.module('myApp').controller('ChildController',['$scope',function($scope){
     
-=======
-"use strict";
-
-angular.module('myApp',[]);
-
-angular.module('myApp').controller('MainController',['$scope','$interval',function($scope,$interval){
-    //part0
-    console.log('Hello!');
-
-    //part1
-    $scope.message = "Hello";
-
-    $scope.sayHello = function(name){
-        return $scope.message + ' ' + name + '.';
-    };
-
-    //part2
-    var items = ['bananas','apples','pears','cherries','peaches'];  // private variable
-    $scope.itemIndex = null;
-    $scope.currentItem = '';
-
-    $scope.getItem = function(){
-        $scope.currentItem = items[$scope.itemIndex];
-    };
-
-    $interval(function(){
-
-        $scope.itemIndex = Math.round(Math.random()*(items.length - 1));
-        $scope.getItem();
-    }, 2000);   //2000 milliseconds
-
-    //part3
-    $scope.randomValue = -999;
-
-    $scope.names = ['David','Thomas','Joe'];
-
-    $scope.qty = 20;
-    $scope.cost = 1.99;
-
-    $scope.pWidth = 100;
-
-    $interval(function(){
-        $scope.randomValue = Math.round(Math.random()*1000000);
-    }, 2000);
-    
-}]);
-    
-//part4
-angular.module('myApp').controller('ParentController',['$scope',function($scope){
-    // $scope.name = 'Nathha Shikari';
-    $scope.model = {
-        name : 'Nathha Shikari'
-    };
-}]);
-angular.module('myApp').controller('ChildController',['$scope',function($scope){
-    
->>>>>>> d9de8fdf651a9639272074a99a5c5076a6900d63
 }]);
